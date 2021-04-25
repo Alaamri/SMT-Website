ccf6dd7aa9dde72c22cba389f9b368577552e5ae
Alaamri <AhmadAlaamri1@gmail.com>
2021-04-25 05:30:43 +0400

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 const toggle = document.querySelector('.toggle')
 const navigation = document.querySelector('.navigation')
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
 toggle.addEventListener('click', () => {
   toggle.classList.toggle('active')
   navigation.classList.toggle('active')
 })
+
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
